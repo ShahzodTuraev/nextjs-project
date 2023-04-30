6234301ed0434301b74ba8663d24851d4c22490a
ShahzodTuraev <turayevshahzodbek@gmail.com>
2023-04-30 17:46:37 +0900

    error page 404 created

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,10 @@
-import Head from 'next/head'
-import style from '../styles/Test.module.css'
-import Link from 'next/link'
 
-export default function Home() {
+const Home = () => {
   return (
-    <>
-    <Head>
-      <title >next Project</title>
-      <meta name ="tutorial" content='this project run in Next Js only to study the framework'></meta>
-    </Head>
-    <div >
-      <h1 className={style.title}>HOME</h1>
-      
-      <ul>
-        <li>
-          <Link href='/post/blok'>
-            Blok
-          </Link>
-        </li>
-      </ul>
-
+    <div>
+      <h1>Home</h1>
     </div>
-    </>
-      
   )
 }
+
+export default Home
